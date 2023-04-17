eecb649c89bba4050f1dec5d8dc71b027fb64aef
dimakovalev <d.nozdryn@gmail.com>
2023-04-17 22:26:45 +0300

    +bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,19 +5,46 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 <body>
+
 <div>
-    <nav>
-        <ul>
-            <li><a href="{{route('main.index')}}">Main</a></li>
-            <li><a href="{{route('contact.index')}}">Contacts</a></li>
-            <li><a href="{{route('post.index')}}">Posts</a></li>
-            <li><a href="{{route('about.index')}}">About</a></li>
-        </ul>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('main.index')}}">Main</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('contact.index')}}">Contacts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('about.index')}}">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
+
+
 </div>
+
 @yield('content')
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 </html>
